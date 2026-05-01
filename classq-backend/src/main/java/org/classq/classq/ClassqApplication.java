30951357fbfd414d3af5706a31686af3fa16bd98
2000JCH <rkwkal59@naver.com>
2026-05-01 23:41:12 +0900

    chore: 초기 환경 구성

---
+++ classq-backend/src/main/java/org/classq/classq/ClassqApplication.java
@@ -0,0 +1,13 @@
+package org.classq.classq;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClassqApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClassqApplication.class, args);
+	}
+
+}
---
+++ classq-backend/src/test/java/org/classq/classq/ClassqApplicationTests.java
@@ -0,0 +1,13 @@
+package org.classq.classq;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClassqApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
