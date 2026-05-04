5c07e21ca4a20b068f7c7bae8a78cb9c70e9f877
2000JCH <rkwkal59@naver.com>
2026-05-04 16:27:45 +0900

    feat: Spring Security 및 JWT 구현

---
+++ classq-backend/src/main/java/org/classq/global/auth/jwt/JwtFilter.java
@@ -0,0 +1,49 @@
+package org.classq.global.auth.jwt;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+import java.util.List;
+
+@RequiredArgsConstructor
+public class JwtFilter extends OncePerRequestFilter {
+
+    private final JwtUtil jwtUtil;
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
+            throws ServletException, IOException {
+
+        String token = resolveToken(request);
+
+        if (token != null && jwtUtil.validateToken(token)) {
+            Long accountId = jwtUtil.getAccountId(token);
+            String role = jwtUtil.getRole(token);
+
+            UsernamePasswordAuthenticationToken authentication = new UsernamePasswordAuthenticationToken(
+                    accountId,  //principal (유저의 id값 등 사용자 정보)
+                    null,   // Credentials (jwt 비밀번호를 이미 검증했으니 null)
+                    List.of(new SimpleGrantedAuthority("ROLE_" + role)) //Authorities (ROLE_USER, ROLE_ADMIN 권한 리스트)
+            );
+            SecurityContextHolder.getContext().setAuthentication(authentication);   //사용자 정보 저장
+        }
+
+        filterChain.doFilter(request, response);
+    }
+
+    private String resolveToken(HttpServletRequest request) {
+        String bearer = request.getHeader("Authorization"); //http헤더에는 Authorization 제외한 다양한 값이 있다. (Content-Type, Host, Cookie등)
+        if (bearer != null && bearer.startsWith("Bearer ")) {
+            return bearer.substring(7); //"Bearer " 접두사를 제거하고 순수한 jwt 값만 추출
+        }
+        return null;
+    }
+}
---
+++ classq-backend/src/main/java/org/classq/global/auth/jwt/JwtUtil.java
@@ -0,0 +1,67 @@
+package org.classq.global.auth.jwt;
+
+import io.jsonwebtoken.JwtException;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import javax.crypto.SecretKey;
+import java.nio.charset.StandardCharsets;
+import java.util.Date;
+
+@Component
+public class JwtUtil {
+
+    private final SecretKey secretKey;  // 마스터 키
+    private final long accessTokenExpiration;   //만료 시간
+
+    public JwtUtil(
+            @Value("${jwt.secret}") String secret,
+            @Value("${jwt.access-token-expiration}") long accessTokenExpiration
+    ) {
+        this.secretKey = Keys.hmacShaKeyFor(secret.getBytes(StandardCharsets.UTF_8));
+        this.accessTokenExpiration = accessTokenExpiration;
+    }
+
+    // 토큰 생성
+    public String generateToken(Long accountId, String role) {
+        Date now = new Date();
+        return Jwts.builder()
+                .subject(String.valueOf(accountId)) // 유저 ID
+                .claim("role", role)    // 커스텀 데이터 (권한 삽입)
+                .issuedAt(now)  // 발급 시간
+                .expiration(new Date(now.getTime() + accessTokenExpiration))    // 만료 시간
+                .signWith(secretKey)    //위의 발급 받을것들을 secretKey를 이용해서 해싱
+                .compact(); // 압축
+    }
+
+    //유효성 검증 (클라이언트가 가져온 토큰이 진짜인지, 유효기간이 지나진 않았는지 확인)
+    public boolean validateToken(String token) {
+        try {
+            Jwts.parser().verifyWith(secretKey).build().parseSignedClaims(token);
+            return true;
+        } catch (JwtException | IllegalArgumentException e) {
+            return false;
+        }
+    }
+
+    // 사용자의 id값을 추출하는 메소드
+    public Long getAccountId(String token) {
+        String subject = Jwts.parser()
+                .verifyWith(secretKey).build()
+                .parseSignedClaims(token)
+                .getPayload()
+                .getSubject();
+        return Long.valueOf(subject);
+    }
+
+    // 사용자의 권한(role)을 추출하는 메소드
+    public String getRole(String token) {
+        return Jwts.parser()
+                .verifyWith(secretKey).build()
+                .parseSignedClaims(token)
+                .getPayload()
+                .get("role", String.class);
+    }
+}
