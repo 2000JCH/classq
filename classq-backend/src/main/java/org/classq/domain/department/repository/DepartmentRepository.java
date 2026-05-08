aafdc873fba3a1e69aee1e5a68fae2459d5b4d9c
2000JCH <rkwkal59@naver.com>
2026-05-08 20:22:26 +0900

    feat: 학과 목록 조회 구현 (GET /api/v1/departments)

---
+++ classq-backend/src/main/java/org/classq/domain/department/repository/DepartmentRepository.java
@@ -0,0 +1,8 @@
+package org.classq.domain.department.repository;
+
+import org.classq.domain.department.entity.Department;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+
+public interface DepartmentRepository extends JpaRepository<Department, Long> {
+}
