655adb9ca9f908187321dcfb5e2820717086e6c9
2000JCH <rkwkal59@naver.com>
2026-05-13 22:46:06 +0900

    feat: phase4 강의 상세 조회 구현

--- classq-backend/src/main/java/org/classq/domain/course/controller/CourseController.java
+++ classq-backend/src/main/java/org/classq/domain/course/controller/CourseController.java
@@ -1,6 +1,7 @@
 package org.classq.domain.course.controller;
 
 import lombok.RequiredArgsConstructor;
+import org.classq.domain.course.dto.CourseDetailDto;
 import org.classq.domain.course.dto.CourseDto;
 import org.classq.domain.course.entity.enums.ClassMode;
 import org.classq.domain.course.entity.enums.ClassType;
@@ -10,6 +11,7 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -21,6 +23,7 @@ public class CourseController {
 
     private final CourseService courseService;
 
+    // 강의 조회
     @GetMapping
     public ResponseEntity<Page<CourseDto>> getCourses(
             @RequestParam(required = false) CourseType courseType,
@@ -31,4 +34,12 @@ public class CourseController {
     ) {
         return ResponseEntity.ok(courseService.getCourses(courseType, classType, classMode, departmentId, pageable));
     }
+
+    // 강의 상세 조회
+    @GetMapping("/{courseId}")
+    public ResponseEntity<CourseDetailDto> getCourseDetail(@PathVariable Long courseId) {
+        return ResponseEntity.ok(courseService.getCourseDetail(courseId));
+    }
+
+
 }
