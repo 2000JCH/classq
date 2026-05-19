f544cee34f52c36a5d387fcd8adb469a006d3a56
2000JCH <rkwkal59@naver.com>
2026-05-19 23:59:57 +0900

    feat: 수강신청 목록 조회 구현 (GET /api/v1/enrollments/me, @Valid/@NotNull 추가)

---
+++ classq-backend/src/main/java/org/classq/domain/enrollment/dto/EnrollmentResponseDto.java
@@ -0,0 +1,17 @@
+package org.classq.domain.enrollment.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import org.classq.domain.enrollment.entity.EnrollmentStatus;
+
+@Getter
+@AllArgsConstructor
+public class EnrollmentResponseDto {
+
+    private Long enrollmentId;
+    private Long courseId;
+    private String courseName;
+    private int credits;
+    private String professorName;
+    private EnrollmentStatus status;
+}
